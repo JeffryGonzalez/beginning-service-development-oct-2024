95429c1fdc538585f87043639954c4d3ad8fb59d
Jeffry Gonzalez <jeff@hypertheory.com>
2024-10-28 14:59:45 -0400

    before break

---
+++ src/HelpDeskSolution/HelpDesk.Api/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// above here (above the builder.Build()) is the behind the scenes configuration of the services our API has.
+var app = builder.Build();
+// after this is configuring how HTTP requests are handled and responses are created.
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers(); // reflection portion - create a "routing table"
+
+
+
+app.Run(); // this is where the application starts running. It is a big while(true) { }
---
+++ src/HelpDeskSolution/HelpDesk.Api/Status/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.Api.Status;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public async Task<ActionResult> GetTheStatus()
+    {
+        return Ok();
+    }
+}
